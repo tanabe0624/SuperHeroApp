2a9e03af306035f296c344377c4601af5faef2a4
RyoTanabe <rtanabe@asx.co.jp>
2024-12-30 08:19:49 +0900

    Fix UI

--- app/src/main/java/com/example/superheroapp/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroapp/HeroesScreen.kt
@@ -2,19 +2,27 @@ package com.example.superheroapp
 
 import android.content.ClipData.Item
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -46,12 +54,17 @@ fun HeroesItem(
     modifier: Modifier = Modifier
 ){
     Card(
-        modifier = modifier
-            .height(104.dp)
-            .padding(16.dp)
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+            //.height(72.dp)
+            //.padding(16.dp)
     ) {
-        Row() {
-            Column() {
+        Row(
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
                 Text(
                     text = LocalContext.current.getString(hero.name),
                     //modifier = modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
@@ -65,11 +78,18 @@ fun HeroesItem(
                 )
             }
             Spacer(Modifier.width(16.dp))
-            Image(
-                painter = painterResource(hero.image),
-                contentDescription = null,
-                contentScale = ContentScale.FillWidth
-            )
+            Box(
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                Image(
+                    painter = painterResource(hero.image),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    alignment = Alignment.TopCenter
+                )
+            }
         }
     }
 
